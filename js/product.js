29c50eb015194f15aab9f29c01d656614c92335f
Haneen Haashlamoun <haneen.hashlamoun@gmail.com>
2021-08-01 19:09:35 +0300

    finish Products Page by Nedal and Haneen

--- js/product.js
+++ js/product.js
@@ -1,14 +1,7 @@
 'use strict';
 
-let max = document.getElementById('maxContainer');
-let image = document.getElementById('imageContainer');
-let list1 = document.getElementById('list1');
-let list2 = document.getElementById('list2');
-let list3 = document.getElementById('list3');
-let list4 = document.getElementById('list4');
-
 // get items from LS
-
+let chosenFilePath = [];
 function getProducts() {
     let data = localStorage.getItem('allProducts');
     let parsedArr = JSON.parse(data);
@@ -17,18 +10,109 @@ function getProducts() {
     }
 }
 
-console.log(Product.all)
-
+let grandDivElement = document.getElementById('grand');
 function renderProducts() {
+    let data = localStorage.getItem('allProducts');
+    let parsedArr = JSON.parse(data);
 
-    for (let i = 0; i < Product.all.length; i++) 
-    {
-        list1.textContent = Product.all[0].name;
-        list2.textContent = Product.all[0].address;
-        list3.textContent = Product.all[0].price;
-        list4.textContent = Product.all[0].rooms;
+
+    for (let i = 0; i < parsedArr.length; i++) {
+        let productDivElement = document.createElement('div');
+        productDivElement.style.display = "inline";
+
+        grandDivElement.appendChild(productDivElement);
+        productDivElement.id = "productDiv" + i;
+
+        let componentsListElement = document.createElement('ul');
+        componentsListElement.id = "componentsList" + i;
+        productDivElement.appendChild(componentsListElement);
+        componentsListElement.style.border = 'solid';
+        componentsListElement.style.padding = "20pt";
+        componentsListElement.style.margin = "20pt";
+
+        let liProductImg = document.createElement('li');
+        liProductImg.id = "productIMGLi" + i;
+
+        componentsListElement.appendChild(liProductImg);
+
+        let productIMG = document.createElement('img');
+        productIMG.id = "img" + i;
+
+        liProductImg.appendChild(productIMG);
+
+        productIMG.src = parsedArr[i].filePath;
+        chosenFilePath.push(parsedArr[i].filePath);
+
+        productIMG.style.width = "450pt";
+        productIMG.style.height = "300pt";
+
+        let featuresDiv = document.createElement('div');
+        featuresDiv.id = "featuresDiv" + i;
+        liProductImg.appendChild(featuresDiv);
+
+        let nameElement = document.createElement('label');
+        nameElement.id = "name" + i;
+        nameElement.textContent = "Name : " + parsedArr[i].name;
+        let breakTag = document.createElement('br');
+        nameElement.appendChild(breakTag);
+        featuresDiv.appendChild(nameElement);
+
+
+        let addressElement = document.createElement('label');
+        addressElement.id = "address" + i;
+        addressElement.textContent = "address : " + parsedArr[i].address;
+        let breakTag2 = document.createElement('br');
+        addressElement.appendChild(breakTag2);
+        featuresDiv.appendChild(addressElement);
+
+        let priceElement = document.createElement('label');
+        priceElement.id = "price" + i;
+        priceElement.textContent = "price : " + parsedArr[i].price;
+        let breakTag3 = document.createElement('br');
+        priceElement.appendChild(breakTag3);
+        featuresDiv.appendChild(priceElement);
+
+        let roomsElement = document.createElement('label');
+        roomsElement.id = "rooms" + i;
+        roomsElement.textContent = "rooms : " + parsedArr[i].rooms;
+        let breakTag4 = document.createElement('br');
+        roomsElement.appendChild(breakTag4);
+        featuresDiv.appendChild(roomsElement);
+
+        let buybtn = document.createElement('button');
+        buybtn.id = i;
+        buybtn.textContent = "ADD TO CART";
+        featuresDiv.appendChild(buybtn);
+
+        // let filePath = parsedArr[i].filePath;
+
+        buybtn.addEventListener('click', clickBtn);
+
+        // document.getElementById(i).onclick = function () {
+        //     location.href = "register.html";
+        // };
+
     }
 }
 
+
+
 getProducts();
 renderProducts();
+
+function clickBtn(event) {
+
+    if (event.target.id !== null) {
+        let chooseIndex = event.target.id;
+        let name = document.getElementById('name' + chooseIndex).textContent;
+        let filePath = chosenFilePath[chooseIndex];
+        // console.log(chooseIndex, name, filePath);
+        SelectedItem.all = [];
+        let item = new SelectedItem(chooseIndex, name, filePath)
+        saveToLocalStorage();
+        // console.log();
+        document.location.href = "register.html";
+
+    }
+
+}
