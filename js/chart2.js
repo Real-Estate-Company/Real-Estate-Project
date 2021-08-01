ed2eca7a19e728baff9150cc7b981feed5515025
suhaibokour <okoursuhaib@gmail.com>
2021-08-01 20:28:41 +0300

    still working on table

--- js/chart2.js
+++ js/chart2.js
@@ -16,11 +16,11 @@ function updateStorage() {
 
 updateStorage();
 
-let parent = document.getElementById('ontrolTable');
-let table = document.createElement('table');
-controlTable.appendChild(table);
+
+
 
 // standing alone functios...
+let parent = document.getElementById('table');
 
 function headerRow() {
   let headingRow = document.createElement('tr');
@@ -28,24 +28,116 @@ function headerRow() {
   let emptyCell = document.createElement('th');
   headingRow.appendChild(emptyCell);
   emptyCell.textContent = 'Name';
+
+  // let paraRow=document.createElement('tr');
+  // headingRow.appendChild(paraRow);
+  // let thPara=document.createElement('th');
+  // paraRow.appendChild(thPara);
+  // thPara.textContent=Product.all[i];
+
+for(let w=0;w<Product.all.length;w++){
+  let thPara=document.createElement('th');
+  headingRow.appendChild(thPara);
+  thPara.textContent=Product[w];
+}
+
+
   for (let i = 0; i < Product.all.length; i++) {
-    let thElement = document.createElement('th');
-    headingRow.appendChild(thElement);
-    thElement.textContent = Product.all[i].name;
-    let firstTd = document.createElement('td');
-    headingRow.appendChild(firstTd);
-    firstTd.textContent = Product.all[i].like;
-    let secondTd = document.createElement('td');
-    headingRow.appendChild(secondTd);
-    secondTd.textContent = Product.all[i].dislike;
-    let lastTd = document.createElement('td');
-    headingRow.appendChild(lastTd);
-    lastTd.textContent = Product.all[i].bought;
-  }
+
+let trElement=document.createElement('tr')
+table.appendChild(trElement);
+trElement.textContent=Product.all[i].name;
+
+let firstTd = document.createElement('td');
+trElement.appendChild(firstTd);
+firstTd.textContent = Product.all[i].name;
+
+let secondTd = document.createElement('td');
+trElement.appendChild(secondTd);
+secondTd.textContent = Product.all[i].filePath;
+
+let thirdTd = document.createElement('td');
+trElement.appendChild(thirdTd);
+thirdTd.textContent = Product.all[i].address;
+
+let forthTd = document.createElement('td');
+trElement.appendChild(forthTd);
+forthTd.textContent = Product.all[i].price;
+
+let fifthTd = document.createElement('td');
+trElement.appendChild(fifthTd);
+fifthTd.textContent = Product.all[i].rooms;
+
+let sixthTd = document.createElement('td');
+trElement.appendChild(sixthTd);
+sixthTd.textContent = Product.all[i].contactus;
+
+let seventhTd = document.createElement('td');
+trElement.appendChild(seventhTd);
+seventhTd.textContent = Product.all[i].like;
+
+let eighthTd = document.createElement('td');
+trElement.appendChild(eighthTd);
+eighthTd.textContent = Product.all[i].dislike;
+
+let ninthTd = document.createElement('td');
+trElement.appendChild(ninthTd);
+ninthTd.textContent = Product.all[i].bought;
+
+
+
+
+  } 
 
 }
 
 headerRow() 
+
+
+
+
+
+
+
+// protoype is not working for making the table !!!!
+
+// Product.prototype.render = function () {
+
+
+
+//   let trElement = document.createElement('tr')
+//   table.appendChild(trElement);
+  
+//   let nameTd = document.createElement('td');
+//   trElement.appendChild(nameTd);
+//   nameTd.textContent = Product.all.name;
+
+// console.log(Product.all);
+
+//   for (let k = 0; k < Product.all.length; k++) {
+
+
+//     let likesTd = document.createElement('td');
+//     table.appendChild(likesTd);
+//     likesTd.textContent = Product.all[k].like;
+//     nameTd.textContent = Product.all[k].name;
+
+
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function productsVotes() {
@@ -56,19 +148,19 @@ function productsVotes() {
 
 
 
+  if (parsedArr !== null) {
+    Product.all = [];
+    for (let i = 0; i < parsedArr.length; i++) {
+      let newProduct = new Product(parsedArr[i].like, parsedArr[i].dislike, parsedArr[i].bought);
+      // newProduct.name = parsedArr[i].name;
 
-  Product.all = [];
-  for (let i = 0; i < parsedArr.length; i++) {
-    let newProduct = new Product(parsedArr[i].like, parsedArr[i].dislike, parsedArr[i].bought);
-    // newProduct.name = parsedArr[i].name;
+      newProduct.like = parsedArr[i].like;
+      newProduct.dislike = parsedArr[i].dislike;
+      newProduct.bought = parsedArr[i].bought;
+      namesArr.push(parsedArr[i].name);
+    }
 
-    newProduct.like = parsedArr[i].like;
-    newProduct.dislike = parsedArr[i].dislike;
-    newProduct.bought = parsedArr[i].bought;
-    namesArr.push(parsedArr[i].name);
   }
-
-
 
 
 
@@ -198,7 +290,7 @@ productsVotes();
 //     let thElement = document.createElement('th');
 //     headingRow.appendChild(thElement);
 //     thElement.textContent = Product.all[i].name;
-   
+
 //     let firstTd = document.createElement('td');
 //     headingRow.appendChild(firstTd);
 //     firstTd.textContent = Product.all[i].like;
@@ -206,7 +298,7 @@ productsVotes();
 //     let secondTd = document.createElement('td');
 //     headingRow.appendChild(secondTd);
 //     secondTd.textContent = Product.all[i].dislike;
-    
+
 //     let lastTd = document.createElement('td');
 //     headingRow.appendChild(lastTd);
 //     lastTd.textContent = Product.all[i].bought;
@@ -224,4 +316,3 @@ productsVotes();
 // }
 
 // headerRow()
-
