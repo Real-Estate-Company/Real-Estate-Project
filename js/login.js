48649c44046be05ff4328d017ce06c9a3cbe6ee7
Haneen Haashlamoun <haneen.hashlamoun@gmail.com>
2021-08-04 00:26:30 +0300

    fixing index and others

--- js/login.js
+++ js/login.js
@@ -2,23 +2,19 @@
 
 //user name prompt
 let username = prompt("Please Enter User Name ?");
-
 userNameValidate(username);
-
 function userNameValidate(username) {
-    while (username.length < 1 || username == null || username == "" || username !=="admin") {
-        let username = prompt("Please Enter User Name ?");
+    while (username == null || username != "admin") {
+        username = prompt("Please Enter Correct User Name ?");        
     }
-    let password = prompt("Please Enter Password ?");
-    PasswordValidate(password);
 }
 
-
+let password = prompt("Please Enter Password ?");
+PasswordValidate(password);
 //Password validation
 function PasswordValidate(password) {
-    while (password.length < 1 || password == null || password == "" || password !== "admin") {
-        let password = prompt("Please Enter Password ?");
+    while (password == null || password != "admin") {
+        password = prompt("Please Enter Correct Password ?");
     }
     alert("welcome to admin control page ..! ");
 }
-
